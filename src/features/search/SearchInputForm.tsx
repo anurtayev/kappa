abf459075aa3564d569f0053cf0c5792af923eb6
Adil Nurtayev <anurtayev@torstar.ca>
2022-05-06 08:54:01 -0400

    added form level validation

--- src/features/search/SearchInputForm.tsx
+++ src/features/search/SearchInputForm.tsx
@@ -1,21 +1,8 @@
-import {
-  ElemBox,
-  ExistingItemsContainer,
-  StyledField,
-} from "features/meta/styles";
-import { Tags } from "features/tags";
 import { Attributes } from "features/attributes";
-import { AttributeSortTerms } from "./AttributeSortTerms";
+import { ElemBox, ExistingItemsContainer } from "features/meta/styles";
+import { Tags } from "features/tags";
+import { Field, FieldArray, Formik } from "formik";
 import {
-  Field,
-  FieldArray,
-  FieldInputProps,
-  FieldMetaProps,
-  Formik,
-  FormikProps,
-} from "formik";
-import {
-  AttributeInput,
   AttributeSortTerm,
   AttributeValueInput,
   Characters,
@@ -27,10 +14,11 @@ import {
   SortOrder,
   useGetAllTagsAndAttributesQuery,
 } from "lib";
+import { array, mixed, object, string, ValidationError } from "yup";
+import { AttributeSortTerms } from "./AttributeSortTerms";
 import { ButtonContainer, FlexForm, SubmitButton } from "./styles";
-import { object, string, array, mixed } from "yup";
 
-const SearchInputFormSchema = object({
+const validationSchema = object({
   filter: object({
     attributes: array().of(
       object({
@@ -49,13 +37,14 @@ const SearchInputFormSchema = object({
       sortOrder: mixed().oneOf([SortOrder.Asc, SortOrder.Desc]),
     })
   ),
-});
-
-type FieldRenderFnProps<Value> = {
-  field: FieldInputProps<Value>;
-  meta: FieldMetaProps<Value>;
-  form: FormikProps<SearchInputFormType>;
-};
+}).test(
+  "global-test",
+  "${path} adsfasdf",
+  ({ filter: { attributes, tags } }, context) =>
+    !!attributes?.length ||
+    !!tags?.length ||
+    new ValidationError("at least one filter must be present", "", "filter")
+);
 
 type SearchInputFormType = {
   filter: {
@@ -114,12 +103,11 @@ export const SearchInputForm = ({ setSearchInput }: SearchInputFormParams) => {
 
         sortInput: { attribute: "", sortOrder: SortOrder.Asc },
       }}
-      onSubmit={({ filter, sorter }, {}) => {
-        console.log("==> ", filter.tags, filter.attributes, sorter);
-
+      onSubmit={({ filter, sorter }, { setSubmitting }) => {
         setSearchInput({ filter, sorter });
+        setSubmitting(false);
       }}
-      validationSchema={SearchInputFormSchema}
+      validationSchema={validationSchema}
     >
       {({
         values: {
@@ -132,189 +120,209 @@ export const SearchInputForm = ({ setSearchInput }: SearchInputFormParams) => {
         isSubmitting,
         setFieldValue,
         errors,
-      }) => (
-        <FlexForm>
-          <div>
-            <p>sorter</p>
+      }) => {
+        return (
+          <FlexForm>
+            <div>
+              {errors.filter &&
+                typeof errors.filter === "string" &&
+                errors.filter}
 
-            <p>tags</p>
-            {errors.filter?.tags}
-            <FieldArray
-              name="filter.tags"
-              render={({ remove, push }) => (
-                <>
-                  <ExistingItemsContainer>
-                    {tags.map((tag, index) => (
-                      <FormBrick key={index}>
-                        <ElemBox>{tag}</ElemBox>
-                        <SmallButton onClick={() => remove(index)}>
-                          {Characters.multiply}
-                        </SmallButton>
-                      </FormBrick>
-                    ))}
-                  </ExistingItemsContainer>
+              <p>sorter</p>
 
-                  <Field name="tagInput" autoComplete="off" />
-                  <button
-                    type="button"
-                    onClick={() => setFieldValue("tagInput", "")}
-                  >
-                    {Characters.multiply}
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      push(tagInput);
-                      setFieldValue("tagInput", "");
-                    }}
-                  >
-                    {Characters.check}
-                  </button>
+              <p>tags</p>
+              {errors.filter?.tags && JSON.stringify(errors.filter?.tags)}
+              <FieldArray
+                name="filter.tags"
+                render={({ remove, push }) => (
+                  <>
+                    <ExistingItemsContainer>
+                      {tags.map((tag, index) => (
+                        <FormBrick key={index}>
+                          <ElemBox>{tag}</ElemBox>
+                          <SmallButton onClick={() => remove(index)}>
+                            {Characters.multiply}
+                          </SmallButton>
+                        </FormBrick>
+                      ))}
+                    </ExistingItemsContainer>
 
-                  <Tags
-                    tags={availableTags
-                      ?.filter(
-                        (availableTag) =>
-                          !tags.includes(availableTag) &&
-                          availableTag.startsWith(tagInput) &&
-                          availableTag !== tagInput
-                      )
-                      .sort()}
-                    onClick={(tag) => push(tag)}
-                  />
-                </>
-              )}
-            />
+                    <Field name="tagInput" autoComplete="off" />
+                    <button
+                      type="button"
+                      onClick={() => setFieldValue("tagInput", "")}
+                    >
+                      {Characters.multiply}
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        push(tagInput);
+                        setFieldValue("tagInput", "");
+                      }}
+                    >
+                      {Characters.check}
+                    </button>
 
-            <p>attributes</p>
-            <FieldArray
-              name="filter.attributes"
-              render={({ remove, push }) => (
-                <>
-                  <ExistingItemsContainer>
-                    {attributes.map((attributeValueInput, index) => (
-                      <FormBrick key={index}>
-                        <ElemBox>{attributeValueInput.attribute.name}</ElemBox>
-                        <ElemBox>{attributeValueInput.value}</ElemBox>
-                        <SmallButton onClick={() => remove(index)}>
-                          {Characters.multiply}
-                        </SmallButton>
-                      </FormBrick>
-                    ))}
-                  </ExistingItemsContainer>
+                    <Tags
+                      tags={availableTags
+                        ?.filter(
+                          (availableTag) =>
+                            !tags.includes(availableTag) &&
+                            availableTag.startsWith(tagInput) &&
+                            availableTag !== tagInput
+                        )
+                        .sort()}
+                      onClick={(tag) => push(tag)}
+                    />
+                  </>
+                )}
+              />
 
-                  <Field name="attributeInput.attribute.name" />
-                  <Field name="attributeInput.attribute.type" as="select">
-                    <option value={InputType.String}>{InputType.String}</option>
-                    <option value={InputType.Number}>{InputType.Number}</option>
-                  </Field>
-                  <Field name="attributeInput.value" />
-                  <button
-                    type="button"
-                    onClick={() =>
-                      setFieldValue("attributeInput", attributeInputInitValue)
-                    }
-                  >
-                    {Characters.multiply}
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      push(attributeInput);
-                      setFieldValue("attributeInput", attributeInputInitValue);
-                    }}
-                  >
-                    {Characters.check}
-                  </button>
+              <p>attributes</p>
+              {JSON.stringify(errors.filter?.attributes)}
+              <FieldArray
+                name="filter.attributes"
+                render={({ remove, push }) => (
+                  <>
+                    <ExistingItemsContainer>
+                      {attributes.map((attributeValueInput, index) => (
+                        <FormBrick key={index}>
+                          <ElemBox>
+                            {attributeValueInput.attribute.name}
+                          </ElemBox>
+                          <ElemBox>{attributeValueInput.value}</ElemBox>
+                          <SmallButton onClick={() => remove(index)}>
+                            {Characters.multiply}
+                          </SmallButton>
+                        </FormBrick>
+                      ))}
+                    </ExistingItemsContainer>
 
-                  <Attributes
-                    attributes={availableAttributes
-                      ?.filter(
-                        (availableAttribute) =>
-                          !attributes.find(
-                            (attributeValueInput: AttributeValueInput) =>
-                              attributeValueInput.attribute.name ===
-                              availableAttribute.name
-                          ) &&
-                          availableAttribute.name.includes(
-                            attributeInput.attribute.name
-                          ) &&
-                          availableAttribute.name !==
-                            attributeInput.attribute.name
-                      )
-                      .sort()}
-                    onClick={(attribute) =>
-                      setFieldValue("attributeInput", { attribute, value: "" })
-                    }
-                  />
-                </>
-              )}
-            />
+                    <Field name="attributeInput.attribute.name" />
+                    <Field name="attributeInput.attribute.type" as="select">
+                      <option value={InputType.String}>
+                        {InputType.String}
+                      </option>
+                      <option value={InputType.Number}>
+                        {InputType.Number}
+                      </option>
+                    </Field>
+                    <Field name="attributeInput.value" />
+                    <button
+                      type="button"
+                      onClick={() =>
+                        setFieldValue("attributeInput", attributeInputInitValue)
+                      }
+                    >
+                      {Characters.multiply}
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        push(attributeInput);
+                        setFieldValue(
+                          "attributeInput",
+                          attributeInputInitValue
+                        );
+                      }}
+                    >
+                      {Characters.check}
+                    </button>
 
-            <p>sorter</p>
-            <FieldArray
-              name="sorter"
-              render={({ remove, push }) => (
-                <>
-                  <ExistingItemsContainer>
-                    {sorter.map((attributeSortTerm, index) => (
-                      <FormBrick key={index}>
-                        <ElemBox>{attributeSortTerm.attribute}</ElemBox>
-                        <SmallButton onClick={() => remove(index)}>
-                          {Characters.multiply}
-                        </SmallButton>
-                      </FormBrick>
-                    ))}
-                  </ExistingItemsContainer>
+                    <Attributes
+                      attributes={availableAttributes
+                        ?.filter(
+                          (availableAttribute) =>
+                            !attributes.find(
+                              (attributeValueInput: AttributeValueInput) =>
+                                attributeValueInput.attribute.name ===
+                                availableAttribute.name
+                            ) &&
+                            availableAttribute.name.includes(
+                              attributeInput.attribute.name
+                            ) &&
+                            availableAttribute.name !==
+                              attributeInput.attribute.name
+                        )
+                        .sort()}
+                      onClick={(attribute) =>
+                        setFieldValue("attributeInput", {
+                          attribute,
+                          value: "",
+                        })
+                      }
+                    />
+                  </>
+                )}
+              />
 
-                  <Field name="sortInput.attribute" />
-                  <Field name="sortInput.sortOrder" as="select">
-                    <option value={SortOrder.Asc}>{SortOrder.Asc}</option>
-                    <option value={SortOrder.Desc}>{SortOrder.Desc}</option>
-                  </Field>
-                  <button
-                    type="button"
-                    onClick={() =>
-                      setFieldValue("sortInput", sortInputInitValue)
-                    }
-                  >
-                    {Characters.multiply}
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => {
-                      push(sortInput);
-                      setFieldValue("sortInput", sortInputInitValue);
-                    }}
-                  >
-                    {Characters.check}
-                  </button>
+              <p>sorter</p>
+              {JSON.stringify(errors.sorter)}
+              <FieldArray
+                name="sorter"
+                render={({ remove, push }) => (
+                  <>
+                    <ExistingItemsContainer>
+                      {sorter.map((attributeSortTerm, index) => (
+                        <FormBrick key={index}>
+                          <ElemBox>{attributeSortTerm.attribute}</ElemBox>
+                          <SmallButton onClick={() => remove(index)}>
+                            {Characters.multiply}
+                          </SmallButton>
+                        </FormBrick>
+                      ))}
+                    </ExistingItemsContainer>
 
-                  <AttributeSortTerms
-                    attributes={availableAttributes
-                      ?.filter(
-                        (availableAttribute) =>
-                          !sorter.find(
-                            (existingAttributeSortTerm) =>
-                              existingAttributeSortTerm.attribute !==
-                              availableAttribute.name
-                          )
-                      )
-                      .sort()}
-                    push={push}
-                  />
-                </>
-              )}
-            />
-          </div>
+                    <Field name="sortInput.attribute" />
+                    <Field name="sortInput.sortOrder" as="select">
+                      <option value={SortOrder.Asc}>{SortOrder.Asc}</option>
+                      <option value={SortOrder.Desc}>{SortOrder.Desc}</option>
+                    </Field>
+                    <button
+                      type="button"
+                      onClick={() =>
+                        setFieldValue("sortInput", sortInputInitValue)
+                      }
+                    >
+                      {Characters.multiply}
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        push(sortInput);
+                        setFieldValue("sortInput", sortInputInitValue);
+                      }}
+                    >
+                      {Characters.check}
+                    </button>
 
-          <ButtonContainer>
-            <SubmitButton type="submit" disabled={isSubmitting}>
-              Search
-            </SubmitButton>
-          </ButtonContainer>
-        </FlexForm>
-      )}
+                    <AttributeSortTerms
+                      attributes={availableAttributes
+                        ?.filter(
+                          (availableAttribute) =>
+                            !sorter.find(
+                              (existingAttributeSortTerm) =>
+                                existingAttributeSortTerm.attribute !==
+                                availableAttribute.name
+                            )
+                        )
+                        .sort()}
+                      push={push}
+                    />
+                  </>
+                )}
+              />
+            </div>
+
+            <ButtonContainer>
+              <SubmitButton type="submit" disabled={isSubmitting}>
+                Search
+              </SubmitButton>
+            </ButtonContainer>
+          </FlexForm>
+        );
+      }}
     </Formik>
   );
 };
