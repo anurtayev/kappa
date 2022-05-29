81ed579d70dcd6ac4504cc415b4690612cba1d6f
Adil Nurtayev <anurtayev@torstar.ca>
2022-05-29 19:26:25 -0400

    changed REACT_APP_CLOUDFRONT_URL to import from CF

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,6 +8,8 @@ fi
 if [ -z $CODEBUILD_CI ]
 then
   export GIT_BRANCH=$(git status|head -n 1|cut -d ' ' -f3)
+else
+  export REACT_APP_CLOUDFRONT_URL=(aws cloudformation describe-stacks --stack-name epsilon-development-092347820172-us-east-1-app --query 'Stacks[0].Outputs[?OutputKey==`CloudFrontDistributionUrl`].OutputValue | [0]')
 fi
 
 export ASPAN_AWS_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text)
