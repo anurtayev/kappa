2a9a7b2561bab1ad4d007a58e5ab612e53bad17f
Adil Nurtayev <anurtayev@torstar.ca>
2022-05-29 19:29:35 -0400

    fixed iffi

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,10 +8,9 @@ fi
 if [ -z $CODEBUILD_CI ]
 then
   export GIT_BRANCH=$(git status|head -n 1|cut -d ' ' -f3)
-else
-  export REACT_APP_CLOUDFRONT_URL=(aws cloudformation describe-stacks --stack-name epsilon-development-092347820172-us-east-1-app --query 'Stacks[0].Outputs[?OutputKey==`CloudFrontDistributionUrl`].OutputValue | [0]')
 fi
 
+export REACT_APP_CLOUDFRONT_URL=(aws cloudformation describe-stacks --stack-name epsilon-development-092347820172-us-east-1-app --query 'Stacks[0].Outputs[?OutputKey==`CloudFrontDistributionUrl`].OutputValue | [0]')
 export ASPAN_AWS_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text)
 export ASPAN_ENV_NAME=$PROJECT-$GIT_BRANCH-$ASPAN_AWS_ACCOUNT-$AWS_DEFAULT_REGION
 export STACK_NAME=$ASPAN_ENV_NAME-webapp
