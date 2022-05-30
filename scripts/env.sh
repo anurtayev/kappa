3db97b80c6c6487c706253feaf0baceb19dfe037
Adil Nurtayev <anurtayev@torstar.ca>
2022-05-29 20:12:23 -0400

    fixed env var

--- scripts/env.sh
+++ scripts/env.sh
@@ -10,7 +10,6 @@ then
   export GIT_BRANCH=$(git status|head -n 1|cut -d ' ' -f3)
 fi
 
-export =$(aws cloudformation describe-stacks --stack-name epsilon-development-092347820172-us-east-1-app --query 'Stacks[0].Outputs[?OutputKey==`CloudFrontDistributionUrl`].OutputValue | [0]' --output text)
 export ASPAN_AWS_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text)
 export ASPAN_ENV_NAME=$PROJECT-$GIT_BRANCH-$ASPAN_AWS_ACCOUNT-$AWS_DEFAULT_REGION
 export STACK_NAME=$ASPAN_ENV_NAME-webapp
