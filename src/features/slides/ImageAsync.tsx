1a0b9e9cd704df8c010e45445e21e865f9ef71f6
Adil Nurtayev <anurtayev@torstar.ca>
2022-06-07 17:44:36 -0400

    implemented image skeleton

--- src/features/slides/ImageAsync.tsx
+++ src/features/slides/ImageAsync.tsx
@@ -1,5 +1,6 @@
 import { createRef, useEffect, useState } from "react";
 import { ImagePane, Image } from "./styles";
+import { Skeleton } from "antd";
 
 export type ImageAsyncParams = { id: string };
 
@@ -11,6 +12,7 @@ export const ImageAsync = ({ id }: ImageAsyncParams) => {
     height: 0,
   });
   const [isMeasured, setIsMeasured] = useState<boolean>(false);
+  const [src, setSrc] = useState<string>("");
 
   useEffect(() => {
     if (frameRef.current && !isMeasured) {
@@ -22,14 +24,57 @@ export const ImageAsync = ({ id }: ImageAsyncParams) => {
     }
   }, [isMeasured]);
 
+  useEffect(() => {
+    setSrc("");
+    isMeasured &&
+      fetch(
+        `${process.env.REACT_APP_CLOUDFRONT_URL}/resizer?key=${id}&width=${measures.width}&height=${measures.height}`
+      )
+        .then((response) => {
+          const reader = response.body?.getReader();
+          return new ReadableStream({
+            start(controller) {
+              return pump();
+              function pump(): any {
+                return reader?.read().then(({ done, value }) => {
+                  if (done) {
+                    controller.close();
+                    return;
+                  }
+                  controller.enqueue(value);
+                  return pump();
+                });
+              }
+            },
+          });
+        })
+        .then((stream) => new Response(stream))
+        .then((response) => response.blob())
+        .then((blob) => URL.createObjectURL(blob))
+        .then((url) => setSrc(url))
+        .catch((err) => console.error(err));
+  }, [id, isMeasured, measures.width, measures.height]);
+
+  console.log("==> ren");
+
   return (
     <ImagePane ref={frameRef}>
-      {isMeasured && (
-        <Image
-          src={`${process.env.REACT_APP_CLOUDFRONT_URL}/resizer?key=${id}&width=${measures.width}&height=${measures.height}`}
-          alt=""
-        />
-      )}
+      {isMeasured &&
+        (src ? (
+          <Image alt="" src={src} />
+        ) : (
+          <div
+            style={{
+              height: "100%",
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Skeleton.Image />
+          </div>
+        ))}
     </ImagePane>
   );
 };
