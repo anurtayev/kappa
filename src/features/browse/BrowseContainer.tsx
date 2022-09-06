524141981e4636d55254a559c18021fa191e2a38
Adil Nurtayev <anurtayev@torstar.ca>
2022-09-06 08:59:22 -0400

    fix(browse): fixed browse page navigation

--- src/features/browse/BrowseContainer.tsx
+++ src/features/browse/BrowseContainer.tsx
@@ -1,4 +1,5 @@
 import {
+  FastBackwardOutlined,
   FastForwardOutlined,
   HomeOutlined,
   RollbackOutlined,
@@ -9,9 +10,9 @@ import { slides } from "cache";
 import { EntriesView } from "features/entries";
 import { Loading } from "features/loading";
 import {
+  AppContext,
   appRoutes,
   getMediaName,
-  AppContext,
   useScrollRef,
   useSlidesQuery,
 } from "lib";
@@ -27,7 +28,8 @@ export const BrowseContainer = () => {
   const pageSize = Number(
     searchParams.get("pagesize") || process.env.REACT_APP_PAGE_SIZE || "20"
   );
-  const nextToken = searchParams.get("nexttoken");
+  const encodedToken = searchParams.get("token");
+  const token = encodedToken ? decodeURIComponent(encodedToken) : encodedToken;
 
   useEffect(() => {
     setTitle(getMediaName(id === process.env.REACT_APP_MEDIA_ROOT ? "" : id));
@@ -37,7 +39,7 @@ export const BrowseContainer = () => {
     variables: {
       pageSize,
       id,
-      nextToken,
+      nextToken: token,
       locationKey,
     },
     context: {
@@ -51,14 +53,21 @@ export const BrowseContainer = () => {
     data?.listFolder?.scrollTop
   );
 
-  const newNextToken = data?.listFolder?.nextToken;
-  const nextPageUrl =
-    newNextToken &&
-    `${appRoutes.browse}?id=${id}&nexttoken=${newNextToken}&pagesize=${pageSize}`;
   const files = data?.listFolder?.files;
   slides(files);
 
+  const nextToken = data?.listFolder?.nextToken;
+
   useEffect(() => {
+    // previous page token for next page
+    // 1st page has no token
+    nextToken &&
+      token &&
+      nextToken !== token &&
+      sessionStorage.setItem(id + nextToken, token);
+
+    const prevToken = token ? sessionStorage.getItem(id + token) : token;
+
     setNavs([
       <Button
         key={"1"}
@@ -72,24 +81,47 @@ export const BrowseContainer = () => {
         icon={<RollbackOutlined />}
         onClick={() => saveScrollTopAndNavigate(-1)}
       />,
-      ...(nextPageUrl
+      ...(token
         ? [
             <Button
               key={"3"}
               shape="circle"
+              icon={<FastBackwardOutlined />}
+              onClick={() =>
+                saveScrollTopAndNavigate(
+                  `${appRoutes.browse}?id=${id}&pagesize=${pageSize}${
+                    prevToken ? `&token=${encodeURIComponent(prevToken)}` : ""
+                  }`
+                )
+              }
+            />,
+          ]
+        : []),
+
+      ...(nextToken
+        ? [
+            <Button
+              key={"4"}
+              shape="circle"
               icon={<FastForwardOutlined />}
-              onClick={() => saveScrollTopAndNavigate(nextPageUrl)}
+              onClick={() =>
+                saveScrollTopAndNavigate(
+                  `${appRoutes.browse}?id=${id}&token=${encodeURIComponent(
+                    nextToken
+                  )}&pagesize=${pageSize}`
+                )
+              }
             />,
           ]
         : []),
       <Button
-        key={"4"}
+        key={"5"}
         shape="circle"
         icon={<SearchOutlined />}
         onClick={() => saveScrollTopAndNavigate(appRoutes.search)}
       />,
     ]);
-  }, [nextPageUrl, saveScrollTopAndNavigate, setNavs]);
+  }, [nextToken, saveScrollTopAndNavigate, setNavs, id, token, pageSize]);
 
   if (loading) return <Loading />;
   if (error) throw error;
@@ -98,7 +130,6 @@ export const BrowseContainer = () => {
     <EntriesView
       folders={data?.listFolder?.folders}
       files={files}
-      nextPageUrl={nextPageUrl}
       scrollTop={data?.listFolder?.scrollTop}
       divRef={divRef}
       saveScrollTopAndNavigate={saveScrollTopAndNavigate}
